b65a639ba7823094766d6b9377ad378cc20a0ac9
Jermiah Jerome <jermiah.jerome96@gmail.com>
2025-02-12 16:31:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,23 +20,26 @@ const translations = {
     sections: {
         overview: {
             title: { en: "Overview", fr: "Aperçu" },
-            content: { 
-                en: `<p>I am a Data Science Enthusiast with five years of experience in data management, product management, and financial markets. My expertise spans across banking and financial markets.</p>
-                
-                <p>Currently pursuing a Master of Science in Data Science and Artificial Intelligence Strategy at Emlyon Business School, I am further honing my expertise in machine learning, deep learning, and large language models (LLMs). My strong academic foundation includes an MBA in Finance & Analytics and a BTech in Computer Science and Engineering, equipping me with both technical proficiency and business acumen.</p>
-                
-                <p>With a diverse skill set encompassing Python, AWS, SQL, PyTorch, Pyspark, VBA/Macros, Advanced Excel, Power Query, and Power BI, I specialize in predictive modeling, deep learning, and AI-driven automation. My past roles include automation, data profiling, data quality, leading data assurance projects, optimizing workflows, and enhancing regulatory compliance at HSBC and BNP Paribas. Notably, my contributions in automating key workflows reduced errors by 95% and processing time by 80%, earning the prestigious Kaizen Award for Innovation.</p>
-                
-                <p>I am also a Certified Scrum Product Owner, skilled in managing agile workflows and product development, ensuring seamless data-driven decision-making processes.</p>`,
-                
-                fr: `<p>Je suis un passionné de science des données avec cinq ans d'expérience en gestion des données, gestion de produit et marchés financiers. Mon expertise couvre la banque, le commerce électronique, la pharmacie et la restauration rapide (QSR), où j'ai relevé des défis complexes liés au risque de crédit, à la modélisation du mix marketing et à l'analyse basée sur l'IA pour fournir des solutions percutantes.</p>
-                
-                <p>Actuellement en train de poursuivre un Master of Science en Science des Données et Stratégie d'Intelligence Artificielle à l'Emlyon Business School, j'affine encore mon expertise en apprentissage automatique, apprentissage profond et modèles de langage de grande envergure (LLMs). Ma solide formation académique comprend un MBA en Finance & Analytics et un BTech en Informatique et Ingénierie, me dotant à la fois de compétences techniques et d'une compréhension approfondie du business.</p>
-                
-                <p>Avec un ensemble de compétences diversifié incluant Python, AWS, SQL, PyTorch, Pyspark, VBA/Macros, Excel avancé, Power Query et Power BI, je me spécialise dans la modélisation prédictive, l'apprentissage profond et l'automatisation basée sur l'IA. Mes précédents rôles impliquent l'automatisation, le profilage des données, la qualité des données, la direction de projets d'assurance qualité des données, l'optimisation des flux de travail et l'amélioration de la conformité réglementaire chez HSBC et BNP Paribas. Notamment, mes contributions à l'automatisation des flux de travail clés ont permis de réduire les erreurs de 95 % et le temps de traitement de 80 %, ce qui m'a valu le prestigieux prix Kaizen pour l'Innovation.</p>
-                
-                <p>Je suis également Certified Scrum Product Owner, compétent dans la gestion des flux de travail agiles et du développement produit, garantissant une prise de décision fluide et basée sur les données.</p>`
-            }
+         {
+    content: { 
+        "en": `<p>I am a Data Science Enthusiast with five years of experience in data management, product management, and financial markets. My expertise spans across banking and financial markets.</p>
+
+        <p>Currently pursuing a Master of Science in Data Science and Artificial Intelligence Strategy at Emlyon Business School, I am further honing my expertise in machine learning, deep learning, and large language models (LLMs). My strong academic foundation includes an MBA in Finance & Analytics and a BTech in Computer Science and Engineering, equipping me with both technical proficiency and business acumen.</p>
+
+        <p>With a diverse skill set encompassing Python, AWS, SQL, PyTorch, Pyspark, VBA/Macros, Advanced Excel, Power Query, and Power BI, I specialize in predictive modeling, deep learning, and AI-driven automation. My past roles include automation, data profiling, data quality, leading data assurance projects, optimizing workflows, and enhancing regulatory compliance at HSBC and BNP Paribas. Notably, my contributions in automating key workflows reduced errors by 95% and processing time by 80%, earning the prestigious Kaizen Award for Innovation.</p>
+
+        <p>I am also a Certified Scrum Product Owner, skilled in managing agile workflows and product development, ensuring seamless data-driven decision-making processes.</p>`,
+        
+        "fr": `<p>Je suis un passionné de science des données avec cinq ans d'expérience en gestion des données, gestion de produit et marchés financiers. Mon expertise couvre la banque et les marchés financiers.</p>
+
+        <p>Actuellement en train de poursuivre un Master of Science en Science des Données et Stratégie d'Intelligence Artificielle à l'Emlyon Business School, j'affine encore mon expertise en apprentissage automatique, apprentissage profond et modèles de langage de grande envergure (LLMs). Ma solide formation académique comprend un MBA en Finance & Analytics et un BTech en Informatique et Ingénierie, me dotant à la fois de compétences techniques et d'une compréhension approfondie du business.</p>
+
+        <p>Avec un ensemble de compétences diversifié incluant Python, AWS, SQL, PyTorch, Pyspark, VBA/Macros, Excel avancé, Power Query et Power BI, je me spécialise dans la modélisation prédictive, l'apprentissage profond et l'automatisation basée sur l'IA. Mes précédents rôles impliquent l'automatisation, le profilage des données, la qualité des données, la direction de projets d'assurance qualité des données, l'optimisation des flux de travail et l'amélioration de la conformité réglementaire chez HSBC et BNP Paribas. Notamment, mes contributions à l'automatisation des flux de travail clés ont permis de réduire les erreurs de 95 % et le temps de traitement de 80 %, ce qui m'a valu le prestigieux prix Kaizen pour l'Innovation.</p>
+
+        <p>Je suis également Certified Scrum Product Owner, compétent dans la gestion des flux de travail agiles et du développement produit, garantissant une prise de décision fluide et basée sur les données.</p>`
+    }
+}
+
         }
     }
 };
